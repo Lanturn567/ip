e66e9e8e8714019415021feeac2129994d08a82b
Andre Keh <andrekeh@gmail.com>
2025-08-15 19:50:12 +0800

    Add Lanturn.java

---
+++ src/main/java/Lanturn.java
@@ -0,0 +1,13 @@
+public class Lanturn {
+    public static String name = "Lanturn";
+    public static void main(String[] args) {
+        System.out.println("--------------------------------------");
+        System.out.println("Hello, I am " + Lanturn.name + "! :D");
+        System.out.println("What can I do for you today?");
+        System.out.println("--------------------------------------");
+
+        System.out.println("Goodbye! Hope to see you again! :D");
+        System.out.println("--------------------------------------");
+
+    }
+}
