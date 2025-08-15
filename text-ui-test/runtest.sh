486778aef15bafc7379bd7e2dd8e5bcd75818666
Andre Keh <andrekeh@gmail.com>
2025-08-15 21:48:33 +0800

    Add automated testing using i/o

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -1,38 +1,34 @@
 #!/usr/bin/env bash
 
 # create bin directory if it doesn't exist
-if [ ! -d "../bin" ]
-then
+if [ ! -d "../bin" ]; then
     mkdir ../bin
 fi
 
 # delete output from previous run
-if [ -e "./ACTUAL.TXT" ]
-then
+if [ -e "./ACTUAL.TXT" ]; then
     rm ACTUAL.TXT
 fi
 
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/*.java
-then
+if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/*.java; then
     echo "********** BUILD FAILURE **********"
     exit 1
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Duke < input.txt > ACTUAL.TXT
+java -classpath ../bin Lanturn < input.txt > ACTUAL.TXT
 
-# convert to UNIX format
+# convert both files to UNIX format to avoid line-ending issues
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
 dos2unix ACTUAL.TXT EXPECTED-UNIX.TXT
 
 # compare the output to the expected output
-diff ACTUAL.TXT EXPECTED-UNIX.TXT
-if [ $? -eq 0 ]
-then
+if diff ACTUAL.TXT EXPECTED-UNIX.TXT > /dev/null; then
     echo "Test result: PASSED"
     exit 0
 else
     echo "Test result: FAILED"
+    diff ACTUAL.TXT EXPECTED-UNIX.TXT
     exit 1
 fi
