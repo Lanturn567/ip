af0ecef42f8e75a800a65e15e4282f69d9e50a8d
Andre Keh <andrekeh@gmail.com>
2025-08-15 19:56:54 +0800

    Add Scanner and command echoing

--- src/main/java/Lanturn.java
+++ src/main/java/Lanturn.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Lanturn {
     public static String name = "Lanturn";
     public static void main(String[] args) {
@@ -5,6 +7,14 @@ public class Lanturn {
         System.out.println("Hello, I am " + Lanturn.name + "! :D");
         System.out.println("What can I do for you today?");
         System.out.println("--------------------------------------");
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+
+        while (!command.equals("bye")) {
+            System.out.println(command);
+            System.out.println("--------------------------------------");
+            command = scanner.nextLine();
+        }
 
         System.out.println("Goodbye! Hope to see you again! :D");
         System.out.println("--------------------------------------");
