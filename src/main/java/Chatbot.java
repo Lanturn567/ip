be6e863eb909960b03076fc9be2e1107a8abdb1a
Andre Keh <andrekeh@gmail.com>
2025-08-15 21:28:03 +0800

    Add Support for 3 tasks in Chatbot

--- src/main/java/Chatbot.java
+++ src/main/java/Chatbot.java
@@ -32,11 +32,38 @@ public class Chatbot {
                 Task curr = this.tasks[num];
                 curr.markUndone();
                 System.out.println("Ok! Marking Task: " + curr.getName() + " as undone!");
-            } else {
-                System.out.println(command);
-                Task task = new Task(command);
+            } else if (command.startsWith("todo")) {
+                int firstSpace = command.indexOf(" ");
+                String desc = command.substring(firstSpace + 1);
+                Todo task = new Todo(desc);
                 this.tasks[this.count] = task;
                 this.count++;
+                System.out.println("Adding Task: " + task.getName() + " to list! :D");
+                System.out.println("Now there are " + this.count + " tasks!");
+            } else if (command.startsWith("deadline")) {
+                int firstSpace = command.indexOf(" ");
+                int firstSlash = command.indexOf(" /by ");
+                String desc = command.substring(firstSpace + 1, firstSlash);
+                String start = command.substring(firstSlash + 5);
+                Deadline task = new Deadline(desc, start);
+                this.tasks[this.count] = task;
+                this.count++;
+                System.out.println("Adding Task: " + task.getName() + " to list! :D");
+                System.out.println("Now there are " + this.count + " tasks!");
+            } else if (command.startsWith("event")) {
+                int firstSpace = command.indexOf(" ");
+                int firstSlash = command.indexOf(" /from ");
+                int secondSlash = command.indexOf(" /to ");
+                String desc = command.substring(firstSpace + 1, firstSlash);
+                String start = command.substring(firstSlash + 7, secondSlash);
+                String end = command.substring(secondSlash + 5);
+                Event task = new Event(desc, start, end);
+                this.tasks[this.count] = task;
+                this.count++;
+                System.out.println("Adding Task: " + task.getName() + " to list! :D");
+                System.out.println("Now there are " + this.count + " tasks!");
+            } else {
+                System.out.println("Invalid format specified... D:");
             }
             System.out.println("--------------------------------------");
             command = scanner.nextLine();
